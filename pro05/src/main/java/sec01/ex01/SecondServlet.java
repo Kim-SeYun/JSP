a832fc25e98fda7bf9d296731e95cfbef2bd7a87
Kim-SeYun <sby06052@naver.com>
2022-11-01 10:39:51 +0900

    add

--- pro05/src/main/java/sec01/ex01/SecondServlet.java
+++ pro05/src/main/java/sec01/ex01/SecondServlet.java
@@ -1,4 +1,4 @@
-package sec00.ex01;
+package sec01.ex01;
 
 import java.io.IOException;
 
@@ -7,23 +7,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MyServlet extends HttpServlet{
+public class SecondServlet extends HttpServlet{
 	
-	public MyServlet() {
+	public SecondServlet() {
 		System.out.println("MyServlet : 서블릿 생성");
 	}
 	
 	@Override
 	public void init() throws ServletException {
-		// 최초 요청시 초기화 작업 수행
 		System.out.println("초기화작업수행");
+		
 	}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// Get방식 요청시 실행
 		System.out.println("doGet() 메소드 실행");
-	
 	}
+	
+	
 
 }
