e7d58488e2dd1f99b56afad00aec8f5fcdc26357
Kim-SeYun <sby06052@naver.com>
2022-11-04 13:54:07 +0900

    add

---
+++ member_pro/src/main/java/dao/MemberDao.java
@@ -0,0 +1,86 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.MemberVO;
+
+public class MemberDao implements IMemberDao {
+	private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
+	private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+	private static final String USER = "scott";
+	private static final String PWD = "tiger";
+	private Connection con; // 오라클에 연동하는데 필요한 객체
+	private PreparedStatement pstmt; // 데이터베이스에 쿼리문 전달
+
+	public List<MemberVO> listMembers() {
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		connDB();
+		String query = "SELECT * FROM T_MEMBER";
+		try {
+			pstmt = con.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery();
+			int i=0;
+			while(rs.next()) {
+				// @param : 테이블 걸럼
+				MemberVO vo = new MemberVO(
+					rs.getString("U_ID"),
+					rs.getString("PWD"),
+					rs.getString("U_NAME"), 
+					rs.getString("EMAIL"),
+					rs.getDate("JOINDATE")); 
+				list.add(vo); // 리스트에 추가
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	// 회원가입
+		public void addMember(MemberVO vo) {
+			connDB();
+			String id = vo.getuId();
+			String pwd = vo.getPwd();
+			String name = vo.getuName();
+			String email = vo.getEmail();
+			String query = "insert into t_member(u_id,pwd,u_name,email) values(?,?,?,?)";
+			try {
+				pstmt = con.prepareStatement(query);
+				pstmt.setString(1, id);
+				pstmt.setString(2, pwd);
+				pstmt.setString(3, name);
+				pstmt.setString(4, email);
+				pstmt.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		private void connDB() {
+			try {
+				Class.forName(DRIVER); // OracleDriver객체 생성
+				// 커넥션 객체를 얻음
+				con = DriverManager.getConnection(URL,USER,PWD);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+
+	@Override
+	public List<MemberVO> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- member_pro/src/main/java/servlet/MemberServlet.java
+++ member_pro/src/main/java/servlet/MemberServlet.java
@@ -1,8 +1,7 @@
-package sec01.ex03;
+package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Date;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -11,12 +10,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.MemberDao;
+import domain.MemberVO;
 
-@WebServlet("/members")
+@WebServlet("/member")
 public class MemberServlet extends HttpServlet {
-       
-   
+	
+	/*
+	 	데이터베이스 테이블 생성
+	 	MemberVO 클래스 : 데이터베이스 테이블 컬럼과 1대1 대응이 되도록(세터게터,생성자,toString)
+	 	MemberDao 클래스 : 
+	      - Connection 객체 얻기 : DataSource 타입 정의
+	      	 - 생성자에서 ConnectionPool을 얻어옴
+	      	 - DataSource로부터 Connection객체를 얻어냄
+	      - PreparedStatement 객체에 쿼리문 전달
+	      - 데이터베이스 테이블에서 조회한 결과를 ResultSet객체로 얻어냄
+	     	 - 결과를 순회하여 MemberVO객체에 데이터를 바인딩함
+	     	 - 리스트에 추가 
+	      - 리소스 반납
+	    MemberServlet : 
+	      - MemberDao 객체 생성 후 필요한 메소드 호출
+	      - 출력
+	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHadle(request,response);
+	
+	}
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHadle(request,response);
+	
+	}
+
+	private void doHadle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
@@ -30,7 +57,7 @@ public class MemberServlet extends HttpServlet {
 			String name = request.getParameter("name");
 			String email = request.getParameter("email");
 			dao.addMember(new MemberVO(id, pwd, name, email));
-			response.sendRedirect("/pro07"); // 지정된 페이지로 이동
+			response.sendRedirect("/member_pro"); // 지정된 페이지로 이동
 			// 아래 코드는 실행되지 않음
 		}else {
 			List<MemberVO> listMembers = dao.listMembers();
@@ -56,11 +83,6 @@ public class MemberServlet extends HttpServlet {
 		}
 		
 	}
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doGet(req, resp);
 	
-	}
-
 	
 }
