46127ab5506ca0d9d91b9190b645c8c4abb9c3bb
Kim-SeYun <sby06052@naver.com>
2022-11-23 16:04:07 +0900

    add

---
+++ pro17/src/main/webapp/resources/js/main.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+	
+	let joinMember = document.querySelector("#joinMember");
+	let message = joinMember.value;
+	if(message !=''){
+		alert(message + '님이 회원가입함');
+	}
+	
+	
+}
